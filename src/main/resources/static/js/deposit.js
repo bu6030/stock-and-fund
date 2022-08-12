161543d043f44f045caf2d4865daf63a22303780
Xuesong.Bu <Xuesong.Bu@homecredit.cn>
2022-08-12 16:15:34 +0800

    修改盈利汇总

--- src/main/resources/static/js/deposit.js
+++ src/main/resources/static/js/deposit.js
@@ -37,24 +37,10 @@ function getTableHtml(result){
         totalStockDayIncome = totalStockDayIncome + parseFloat(result[k].stockDayIncome);
         totalTotalDayIncome = totalTotalDayIncome + parseFloat(result[k].totalDayIncome);
 
-        totalFundMarketValue = totalFundMarketValue + parseFloat(result[k].fundMarketValue);
-        totalStockMarketValue = totalStockMarketValue + parseFloat(result[k].stockMarketValue);
-        totalTotalMarketValue = totalTotalMarketValue + parseFloat(result[k].totalMarketValue);
-
         var fundIncomePercent = parseFloat(result[k].fundDayIncome).toFixed(4) * 100/parseFloat(result[k].fundMarketValue);
         var stockIncomePercent = parseFloat(result[k].stockDayIncome).toFixed(4) * 100/parseFloat(result[k].stockMarketValue);
         var totalIncomePercent = parseFloat(result[k].totalDayIncome).toFixed(4) * 100/parseFloat(result[k].totalMarketValue);
-        // var fundIncomePercent = new BigDecimal("0");
-        // fundIncomePercent = (new BigDecimal(totalFundDayIncome+"")).multiply(new BigDecimal("100")).divide(new BigDecimal(totalFundMarketValue+""));
-        // var stockIncomePercent = new BigDecimal("0");
-        // stockIncomePercent = (new BigDecimal(totalStockDayIncome+"")).multiply(new BigDecimal("100")).divide(new BigDecimal(totalStockMarketValue+""));
-        //
-        // alert((new BigDecimal(totalStockDayIncome+"")));
-        // alert((new BigDecimal(totalStockDayIncome+"")).multiply(new BigDecimal("100")));
-        // alert((new BigDecimal(totalStockDayIncome+"")).multiply(new BigDecimal("100")).divide(new BigDecimal(totalStockMarketValue+"")));
 
-        // var totalIncomePercent = new BigDecimal("0");
-        // totalIncomePercent = (new BigDecimal(totalTotalDayIncome+"")).multiply(new BigDecimal("100")).divide(new BigDecimal(totalTotalMarketValue+""));
         str += "<tr><td>" + result[k].date
             + "</td><td>" + parseFloat(result[k].fundDayIncome).toFixed(2)
             + "</td><td>" + parseFloat(result[k].fundMarketValue).toFixed(2)
@@ -70,19 +56,17 @@ function getTableHtml(result){
             +"</td></tr>";
 
     }
-    var totalIncomePercent = totalTotalDayIncome * 100 / totalTotalMarketValue;
-    var totalFundIncomePercent = totalFundDayIncome * 100 / totalFundMarketValue;
-    var totalStockIncomePercent = totalStockDayIncome* 100 / totalStockMarketValue;
+
     str += "<tr><td>合计"
         + "</td><td>" + parseFloat(totalFundDayIncome).toFixed(2)
-        + "</td><td>" + parseFloat(totalFundMarketValue).toFixed(2)
-        + "</td><td>" + parseFloat(totalFundIncomePercent).toFixed(2) + "%"
+        + "</td><td>"
+        + "</td><td>"
         + "</td><td>" + parseFloat(totalStockDayIncome).toFixed(2)
-        + "</td><td>" + parseFloat(totalStockMarketValue).toFixed(2)
-        + "</td><td>" + parseFloat(totalStockIncomePercent).toFixed(2) + "%"
+        + "</td><td>"
+        + "</td><td>"
         + "</td><td>" + parseFloat(totalTotalDayIncome).toFixed(2)
-        + "</td><td>" + parseFloat(totalTotalMarketValue).toFixed(2)
-        + "</td><td>" + parseFloat(totalIncomePercent).toFixed(2) + "%"
+        + "</td><td>"
+        + "</td><td>"
         +"</td></tr>";
     return str;
 }
