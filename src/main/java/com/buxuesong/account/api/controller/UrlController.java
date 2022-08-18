a3abad15a0da1774021cf9c57cea625f3a02f17b
Xuesong.Bu <Xuesong.Bu@homecredit.cn>
2022-08-18 11:03:17 +0800

    layui新增字典页面

--- src/main/java/com/buxuesong/account/api/controller/UrlController.java
+++ src/main/java/com/buxuesong/account/api/controller/UrlController.java
@@ -26,6 +26,9 @@ public class UrlController {
     @Autowired
     private ParamService paramService;
 
+
+    // 老版本页面，BootStrap版本
+
     /**
      * 基金页面初始化
      *
@@ -106,6 +109,63 @@ public class UrlController {
     }
 
     /**
+     * 收益汇总页面初始化
+     *
+     * @return
+     */
+    @GetMapping(value = "/deposit.html")
+    public ModelAndView depositInit(HttpServletRequest request, HttpServletResponse response) {
+        ModelAndView m = new ModelAndView();
+        m.setViewName("deposit");
+        return m;
+    }
+
+    /**
+     * 字典数据页面初始化
+     *
+     * @return
+     */
+    @GetMapping(value = "/param.html")
+    public ModelAndView paramInit(HttpServletRequest request, HttpServletResponse response) {
+        ModelAndView m = new ModelAndView();
+        m.setViewName("param");
+        return m;
+    }
+
+    /**
+     * 添加字典数据页面初始化
+     *
+     * @return
+     */
+    @GetMapping(value = "/addParam.html")
+    public ModelAndView addParamInit(@RequestParam String type, HttpServletRequest request, HttpServletResponse response) {
+        ModelAndView m = new ModelAndView();
+        m.setViewName("addParam");
+        m.getModel().put("type", type);
+        return m;
+    }
+
+    /**
+     * 修改股票基金页面初始化
+     *
+     * @return
+     */
+    @GetMapping(value = "/updateParam.html")
+    public ModelAndView updateParamInit(@RequestParam String type, @RequestParam String code, HttpServletRequest request,
+                                        HttpServletResponse response) {
+        log.info("修改字典参数初始化 type : {}, code : {}", type, code);
+        ModelAndView m = new ModelAndView();
+        m.setViewName("updateParam");
+        Parameter parameter = paramService.getParamByTypeAndCode(Parameter.builder().code(code).type(type).build());
+        log.info("修改字典参数初始化 parameter : {}", parameter);
+        m.getModel().put("code", parameter.getCode());
+        m.getModel().put("type", parameter.getType());
+        m.getModel().put("name", parameter.getName());
+        return m;
+    }
+
+    // 新版本页面，layui版本
+    /**
      * 新基金页面初始化，引入layui
      *
      * @return
@@ -142,18 +202,6 @@ public class UrlController {
     }
 
     /**
-     * 收益汇总页面初始化
-     *
-     * @return
-     */
-    @GetMapping(value = "/deposit.html")
-    public ModelAndView depositInit(HttpServletRequest request, HttpServletResponse response) {
-        ModelAndView m = new ModelAndView();
-        m.setViewName("deposit");
-        return m;
-    }
-
-    /**
      * 新收益汇总页面初始化，引入layui
      *
      * @return
@@ -170,42 +218,10 @@ public class UrlController {
      *
      * @return
      */
-    @GetMapping(value = "/param.html")
-    public ModelAndView paramInit(HttpServletRequest request, HttpServletResponse response) {
+    @GetMapping(value = "/paramNew.html")
+    public ModelAndView paramNewInit(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView m = new ModelAndView();
-        m.setViewName("param");
-        return m;
-    }
-
-    /**
-     * 添加字典数据页面初始化
-     *
-     * @return
-     */
-    @GetMapping(value = "/addParam.html")
-    public ModelAndView addParamInit(@RequestParam String type, HttpServletRequest request, HttpServletResponse response) {
-        ModelAndView m = new ModelAndView();
-        m.setViewName("addParam");
-        m.getModel().put("type", type);
-        return m;
-    }
-
-    /**
-     * 修改股票基金页面初始化
-     *
-     * @return
-     */
-    @GetMapping(value = "/updateParam.html")
-    public ModelAndView updateParamInit(@RequestParam String type, @RequestParam String code, HttpServletRequest request,
-        HttpServletResponse response) {
-        log.info("修改字典参数初始化 type : {}, code : {}", type, code);
-        ModelAndView m = new ModelAndView();
-        m.setViewName("updateParam");
-        Parameter parameter = paramService.getParamByTypeAndCode(Parameter.builder().code(code).type(type).build());
-        log.info("修改字典参数初始化 parameter : {}", parameter);
-        m.getModel().put("code", parameter.getCode());
-        m.getModel().put("type", parameter.getType());
-        m.getModel().put("name", parameter.getName());
+        m.setViewName("paramNew");
         return m;
     }
 }
