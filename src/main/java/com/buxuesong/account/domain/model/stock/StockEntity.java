2818f05891c511fd70665f957bc403a4beb6ef6e
Xuesong.Bu <Xuesong.Bu@homecredit.cn>
2023-04-12 15:06:27 +0800

    修改md内容

--- src/main/java/com/buxuesong/account/domain/model/stock/StockEntity.java
+++ src/main/java/com/buxuesong/account/domain/model/stock/StockEntity.java
@@ -500,14 +500,12 @@ public class StockEntity {
         int successCnt = 0;
         // 从第21天开始计算
         for (int i = 20; i < stockDayHistory.size(); i++) {
-            List<StockDayHistoryResponse> stockDayHistory20 = stockDayHistory.subList(i - 20, i).stream().map(s -> s.clone())
-                .collect(Collectors.toList());
-            List<StockDayHistoryResponse> stockDayHistory10 = stockDayHistory.subList(i - 10, i).stream().map(s -> s.clone())
-                .collect(Collectors.toList());
+            List<StockDayHistoryResponse> stockDayHistory20 = stockDayHistory.subList(i - 20, i);
+            List<StockDayHistoryResponse> stockDayHistory10 = stockDayHistory.subList(i - 10, i);
             currentStockDay = stockDayHistory.get(i);
-            StockDayHistoryResponse maxStockDay = stockDayHistory20.stream().max((s1, s2) -> Double.compare(s1.getHigh(), s2.getHigh()))
+            StockDayHistoryResponse maxStockDay = stockDayHistory20.stream().max(Comparator.comparingDouble(StockDayHistoryResponse::getHigh))
                 .get();
-            StockDayHistoryResponse minStockDay = stockDayHistory10.stream().min((s1, s2) -> Double.compare(s1.getLow(), s2.getLow()))
+            StockDayHistoryResponse minStockDay = stockDayHistory10.stream().min(Comparator.comparingDouble(StockDayHistoryResponse::getLow))
                 .get();
             // 未持有，并且突破20日新高，买入
             if (!haveOne && currentStockDay.getHigh() > maxStockDay.getHigh()) {
