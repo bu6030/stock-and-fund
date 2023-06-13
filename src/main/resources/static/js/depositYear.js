0f1a18cd6785c37c4d07ce55b0f40d692b17668b
Xuesong.Bu <Xuesong.Bu@homecredit.cn>
2023-06-13 14:06:17 +0800

    增加年度月度收益汇总图表

--- src/main/resources/static/js/depositYear.js
+++ src/main/resources/static/js/depositYear.js
@@ -47,10 +47,9 @@ function getTableHtml(result){
     var totalTotalDayIncome = 0;
     var totalFundDayIncome = 0;
     var totalStockDayIncome = 0;
+    let totalIncomes = [];
+    let days = [];
 
-    var totalFundMarketValue = 0;
-    var totalStockMarketValue = 0;
-    var totalTotalMarketValue = 0;
     for(var k in result) {
         totalFundDayIncome = totalFundDayIncome + parseFloat(result[k].fundDayIncome);
         totalStockDayIncome = totalStockDayIncome + parseFloat(result[k].stockDayIncome);
@@ -59,6 +58,9 @@ function getTableHtml(result){
         var stockDayIncomeStyle = result[k].stockDayIncome == 0 ? "" : (result[k].stockDayIncome > 0?"style=\"color:#c12e2a\"":"style=\"color:#3e8f3e\"");
         var fundDayIncomeStyle = result[k].fundDayIncome == 0 ? "" : (result[k].fundDayIncome > 0?"style=\"color:#c12e2a\"":"style=\"color:#3e8f3e\"");
         var totalDayIncomeStyle = result[k].totalDayIncome == 0 ? "" : (result[k].totalDayIncome > 0?"style=\"color:#c12e2a\"":"style=\"color:#3e8f3e\"");
+
+        totalIncomes.push(parseFloat(result[k].totalDayIncome).toFixed(2));
+        days.push(result[k].date);
 
         str += "<tr><td>" + result[k].date
             + "</td><td " + fundDayIncomeStyle + ">" + parseFloat(result[k].fundDayIncome).toFixed(2)
@@ -74,5 +76,62 @@ function getTableHtml(result){
         + "</td><td>" + parseFloat(totalTotalDayIncome).toFixed(2)
         + "</td></tr>"
         + str;
+
+    // 展示报表
+    var type = "收益汇总";
+    var dataType = ['收益汇总'];
+    var seriesData = [];
+    seriesData[0] = {
+        name: "合计",
+        type: 'bar',
+        data: totalIncomes.reverse()
+    };
+    var color = ['#2378f7'];
+    setDetailChart(type, dataType, days.reverse(), seriesData, color);
+
     return newStr;
+}
+
+function setDetailChart(type, dataType, dataAxis, seriesData, color){
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('detailContainer'));
+
+    option = {
+        tooltip : {
+            trigger: 'axis'
+        },
+        title: {
+            text: type
+        },
+        legend: {
+            data:dataType
+        },
+        toolbox: {
+            show : true,
+            feature : {
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+                magicType : {show: true, type: [ 'bar', 'line', 'stack', 'tiled']},
+                restore : {show: true},
+                saveAsImage : {show: true}
+            }
+        },
+        // color : ['#8cc540','#009f5d','#019fa0','#019fde','#007cdc','#887ddd','#cd7bdd','#ff5675','#ff1244','#ff8345','#f8bd0b','#d1d2d4'],
+        color : color,
+        calculable : true,
+        xAxis : [
+            {
+                type : 'category',
+                data : dataAxis
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value'
+            }
+        ],
+        series : seriesData
+    };
+
+    myChart.setOption(option);
 }
