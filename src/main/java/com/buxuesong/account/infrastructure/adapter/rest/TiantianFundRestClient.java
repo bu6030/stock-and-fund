a4905b26c6b82eb58d5e620670dc74eef1006614
bu6030 <buxuesong@foxmail.com>
2024-08-28 21:18:18 +0800

    天天基金接口返回异常处理

--- src/main/java/com/buxuesong/account/infrastructure/adapter/rest/TiantianFundRestClient.java
+++ src/main/java/com/buxuesong/account/infrastructure/adapter/rest/TiantianFundRestClient.java
@@ -7,6 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
 
 @Slf4j
 @Service
@@ -16,20 +21,42 @@ public class TiantianFundRestClient {
 
     private static final String GET_FUND_INFO_URL = "http://fundgz.1234567.com.cn/js/{code}.js";
 
+//    public String getFundInfo(String code) {
+//        log.info("通过天天基金接口获取基金，编码：{}， URL：{}", code, GET_FUND_INFO_URL);
+//        String url = UriComponentsBuilder.fromUriString(GET_FUND_INFO_URL)
+//            .queryParam("rt", System.currentTimeMillis())
+//            .build()
+//            .toUriString();
+//        ResponseEntity<String> response = null;
+//        try {
+//            response = restTemplate.exchange(
+//                url, HttpMethod.GET, null, String.class, code);
+//        } catch (Exception e) {
+//            log.info("获取天天基金接口异常: {]", e);
+//            return null;
+//        }
+//        return response.getBody();
+//    }
+
     public String getFundInfo(String code) {
         log.info("通过天天基金接口获取基金，编码：{}， URL：{}", code, GET_FUND_INFO_URL);
-        String url = UriComponentsBuilder.fromUriString(GET_FUND_INFO_URL)
-            .queryParam("rt", System.currentTimeMillis())
-            .build()
-            .toUriString();
-        ResponseEntity<String> response = null;
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(GET_FUND_INFO_URL.replace("{code}", code) + "?rt=" + System.currentTimeMillis()))
+                .header("Accept", "application/javascript")
+                .GET()
+                .build();
         try {
-            response = restTemplate.exchange(
-                url, HttpMethod.GET, null, String.class, code);
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString(Charset.forName("UTF-8")));
+            if (response.statusCode() == 200) {
+                return response.body();
+            } else {
+                log.warn("获取天天基金接口异常 HTTP status code: {}", response.statusCode());
+                return null;
+            }
         } catch (Exception e) {
-            log.info("获取天天基金接口异常: {]", e);
+            log.info("获取天天基金接口异常: {}", e.getMessage());
             return null;
         }
-        return response.getBody();
     }
 }
